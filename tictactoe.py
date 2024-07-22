6c4ecde57866bfa1178ac1943ee3cd2624e40473
Khair-Un-Nisa05 <bintearif2004@gmail.com>
2024-07-22 11:25:25 -0700

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -28,13 +28,12 @@ def  checkWin (xState, zState):
         print("O Won the match")
         return 0
      return -1
-if __name__ == "__main__":
-    
-    xState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    zState = [0, 0 ,0, 0, 0, 0, 0, 0, 0]
-    turn = 1 #1 for X and 0 for O
-    print("Welcome to Tic Tac Toe")
-    while(True):
+
+   xState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+   zState = [0, 0 ,0, 0, 0, 0, 0, 0, 0]
+   turn = 1 #1 for X and 0 for O
+   print("Welcome to Tic Tac Toe")
+   while(True):
        printBoard(xState, zState)
        if(turn == 1):
           print("X's Chance")
